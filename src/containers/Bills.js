bb5836e2d43284799207a78462b4f158dd8709c3
Ingo Knorr <ingoknorr@gmail.com>
2023-07-09 15:32:22 +0200

    bills are now properly displayed in with newest date on top

--- src/containers/Bills.js
+++ src/containers/Bills.js
@@ -45,9 +45,9 @@ export default class {
             const dateA = new Date(a.date);
             const dateB = new Date(b.date);
 
-            if (dateA < dateB) {
+            if (dateA > dateB) {
               return -1;
-            } else if (dateA > dateB) {
+            } else if (dateA < dateB) {
               return 1;
             } else {
               return 0;
